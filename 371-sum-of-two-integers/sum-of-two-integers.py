912698fdf867ef5dfeb369ec3b17306e89682aa1
Asish Jose <132321616+asishjose@users.noreply.github.com>
2025-12-02 16:15:31 +0530

    Time: 4 ms (0.44%) | Memory: 17.6 MB (77.44%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,3 +1,13 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        return a+b
+        MASK = 0xFFFFFFFF  
+        MAX_INT = 0x7FFFFFFF  
+
+        x = a & MASK
+        y = b & MASK
+
+        while y != 0:
+            car = (x & y) << 1
+            x = x ^ y
+            y = car & MASK  
+        return x if x <= MAX_INT else ~(x ^ MASK)
