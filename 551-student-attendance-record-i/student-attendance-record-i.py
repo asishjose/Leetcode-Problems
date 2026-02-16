14025ecd157719fca143ea750093a4e6c2423d0f
Asish Jose <132321616+asishjose@users.noreply.github.com>
2026-02-16 10:20:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.65%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.py
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -1,17 +1,15 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
         eligibility = 1
-        if s.count('A')>1:
-            eligibility = 0
+        abscence_count = 0
         late_count = 0
         for day in s:
             if day=='L':
                 late_count+=1
             else:
                 late_count=0
-            if late_count==3:
+            if day=='A':
+                abscence_count+=1
+            if late_count==3 or abscence_count>1:
                 eligibility = 0
-        if eligibility==1:
-            return True
-        else:
-            return False
+        return eligibility==1
