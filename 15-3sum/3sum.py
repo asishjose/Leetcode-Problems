4b10cde05628a2875513d1b66c6007bbf6f9d318
Asish Jose <132321616+asishjose@users.noreply.github.com>
2026-02-19 13:26:33 +0530

    Time: 659 ms (34.64%) | Memory: 22.3 MB (58.38%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,31 +3,27 @@ class Solution:
         nums.sort()
         res = []
         for i in range(len(nums)):
-            if i > 0 and nums[i]==nums[i-1]: #avoid duplicate anchors
+            if i >0 and nums[i]==nums[i-1]:
                 continue
             
-            # Two Pointers on rest of the list
             left = i+1
             right = len(nums)-1
-
+            
             while left<right:
                 curr_sum = nums[i]+nums[left]+nums[right]
+
                 if curr_sum == 0:
                     res.append([nums[i], nums[left], nums[right]])
-
-                    left+=1
-                    right-=1
+                    left += 1
+                    right -= 1
 
                     while left<right and nums[left]==nums[left-1]:
-                        left+=1
+                        left += 1
                     while left<right and nums[right]==nums[right+1]:
-                        right-=1
+                        right -= 1
 
                 elif curr_sum < 0:
-                    left+=1
+                    left += 1
                 else:
-                    right-=1
+                    right -= 1
         return res
-
-            
-            
