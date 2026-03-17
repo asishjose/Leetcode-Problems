70d61214faad73cf4936e214e65aac502370c96f
Asish Jose <132321616+asishjose@users.noreply.github.com>
2026-03-17 11:55:47 +0530

    Time: 55 ms (46.42%) | Memory: 31.4 MB (88.60%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -1,13 +1,13 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         arr.sort()
-        min_diff = float('inf')
+        min_diff = 999999999999
+        for i in range(1,len(arr)):
+            diff = arr[i] - arr[i-1]
+            min_diff = min(diff, min_diff)
         res = []
-        for i in range(len(arr)-1):
-            diff = arr[i+1] - arr[i]
-            if diff < min_diff:
-                min_diff = diff
-                res = [[arr[i], arr[i+1]]]
-            elif diff == min_diff:
-                res.append([arr[i], arr[i+1]])
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i-1]
+            if diff==min_diff:
+                res.append([arr[i-1], arr[i]])
         return res
