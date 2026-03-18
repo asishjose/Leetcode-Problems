4d56273ba00833a80f28c28bfee63c9ad8ef8b87
Asish Jose <132321616+asishjose@users.noreply.github.com>
2026-03-18 10:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,7 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        la = 1
-        s_la = 1
-        for i in range(1,n):
+        la, s_la = 1, 1
+        for i in range(n-1):
             la, s_la = la + s_la, la
         return la
