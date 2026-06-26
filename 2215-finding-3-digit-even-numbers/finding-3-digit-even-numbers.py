6534cde75b5ccd23f43310d7c5ef374ed365a163
Asish Jose <132321616+asishjose@users.noreply.github.com>
2026-06-26 15:15:17 +0530

    Time: 2145 ms (44.52%) | Memory: 19.4 MB (51.96%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -1,24 +1,21 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        res = []
-        return self.checkNumbers(100, digits, res)
-
-    def checkNumbers(self, n, digits, res):
-        if n > 998:
-            return res
-
-        if self.numCheck(n, digits):
-            res.append(n)
-
-        return self.checkNumbers(n + 2, digits, res)
-
-    def numCheck(self, n, digits):
-        digitss = digits.copy()
-
-        for d in map(int, str(n)):
-            if d in digitss:
-                digitss.remove(d)
-            else:
-                return False
-
-        return True
+        res = set()
+        n = len(digits)
+        for i in range(n):
+            if digits[i]==0:
+                continue
+            for j in range(n):
+                if i==j:
+                    continue
+                for k in range(n):
+                    if i==k or j==k:
+                        continue
+                    if digits[k]%2!=0:
+                        continue
+                    res.add( digits[i]*100 +
+                                digits[j]*10  +
+                                digits[k]
+                            )
+        return sorted(res)
+        
